1f792ae415e5084bc371ae0571eec15a6064ad34
Bilal Aslam <bilalmalik9851@yahoo.com>
2024-01-20 19:38:38 +0500

    Add: Mutable and immutable data types in javascript

---
+++ Hoisting/example.js
@@ -0,0 +1,15 @@
+/// example #1 ///
+
+console.log(test) // Undefined
+
+function testFunc() {
+   console.log(test)
+}
+
+testFunc() // undefined
+
+var test = "test"
+
+testFunc() // "test"
+
+// //
---
+++ Mutablility and Immutability/example.js
@@ -0,0 +1,6 @@
+let obj = { key: "value" };
+
+// Mutating the object
+obj.key = "new value";
+
+console.log(obj.key); // Outputs: "new value"
